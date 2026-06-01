ca82650651603a81546f224df081aa6f73cbe5ce
VaricelaZoster <maildwaipayansaha@gmail.com>
2026-06-02 01:50:38 +0530

    added bouncing text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -97,6 +97,14 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      <div className="hidden md:block absolute bottom-8 left-8 lg:bottom-10 lg:left-10 animate-bounce">
+        <div className="bg-neutral-900 text-white  px-2.5 py-1.5 rounded-lg text-xm lg:text-sm flex items-center shadow-lg">
+          <i className="bx bx-check-circle mr-1 text-amber-500"></i>Responsive</div>
+      </div>
+      <div className="hidden md:block absolute top-16 right-[15%] lg:top-20 lg:right-[20%] animate-bounce delay-300">
+        <div className="bg-neutral-900 text-white  px-2.5 py-1.5 rounded-lg text-xm lg:text-sm flex items-center shadow-lg">
+          <i className="bx bx-check-circle mr-1 text-amber-500"></i>Accesible</div>
+      </div>
     </div>
   )
 }
